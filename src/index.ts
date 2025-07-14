bbc1a1f9558422f2c3b535fad31ab7c1b1251ab5
Sameer Katija <sameerkatija@gmail.com>
2025-07-15 00:44:12 +0500

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Express } from "express";
 import { PORT } from "./config/env";
+import cors from "cors";
 import indexRoute from "./routes/index";
 const app: Express = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", indexRoute);
---
+++ src/routes/admin.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+const router = Router();
+
+export default router;
+
+// /admin/dashboard
+
+// /admin/users
+
+// /admin/approve-teacher
+
+// /admin/courses
+
+// /admin/analytics (optional)
